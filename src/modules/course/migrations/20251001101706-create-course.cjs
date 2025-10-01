1c6f3bea21040cdcac0740c4095ccbdbdf099005
Simon <ogbanjesi@gmail.com>
2025-10-01 11:33:50 +0100

    Implement course management features with CRUD operations and views

---
+++ src/modules/course/migrations/20251001101706-create-course.cjs
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('courses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      image_url: {
+        type: Sequelize.STRING
+      },
+      duration: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+        comment: 'Duration in weeks'
+
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('courses');
+  }
+};
---
+++ src/modules/course/models/course.cjs
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Course.init({
+    title: {
+      type: DataTypes.STRING
+    },
+    price: {
+      type: DataTypes.DECIMAL
+    },
+    description: {
+      type: DataTypes.TEXT
+    },
+    image_url: {
+      type: DataTypes.STRING
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+      comment: 'Duration in weeks'
+
+    },
+  }, {
+    sequelize,
+    modelName: 'Course',
+    tableName: 'courses',
+  });
+  return Course;
+};
